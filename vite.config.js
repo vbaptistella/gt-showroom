1228861c047f192d4cb23605e8e016935e3ddb4e
Victor Baptistella <baptistellav@gmail.com>
2024-04-09 07:01:50 -0300

    Changed vite build mode and bumped version

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: "./src/main.jsx",
-    },
-  },
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000/",
-    },
-  },
-});
+})
