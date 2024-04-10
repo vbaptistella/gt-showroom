f74ff93dba1fd77f6e0ef45628107f65a1876edc
Victor Baptistella <baptistellav@gmail.com>
2024-04-10 13:57:11 -0300

    Updated build dir to work with railway

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -8,12 +8,16 @@ export default function MainMenu() {
       <Link to={`/new`}>
         <div className="card-button">New Cars</div>
       </Link>
-      <Link to={`/used`}>
-        <div className="card-button">Used Cars</div>
-      </Link>
-      <Link to={`/classic`}>
-        <div className="card-button">Classic Cars</div>
-      </Link>
+      {/* <Link to={`/used`}> */}
+      <div className="card-button disabled">
+        Used Cars<i>Coming soon</i>
+      </div>
+      {/* </Link> */}
+      {/* <Link to={`/classic`}> */}
+      <div className="card-button disabled">
+        Classic Cars<i>Coming soon</i>
+      </div>
+      {/* </Link> */}
     </div>
   );
 }
